534f9dd5a4c709672dba1bbf6849558f09c83e47
ToskePG <danilo.tosic.pmf@gmail.com>
2024-07-18 01:49:57 +0200

    Calls the index template for the root page.

--- app/main.py
+++ app/main.py
@@ -6,4 +6,13 @@ from .database import models
 models.Base.metadata.create_all(bind=engine)
 app =FastAPI()
 
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Mount static files if you have any
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(user.router)
