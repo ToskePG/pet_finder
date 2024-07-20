95f9c7d3113fce6615ce790b948f1660635de854
toskepg <danilo.tosic.pmf@gmail.com>
2024-07-20 05:03:08 +0200

    Starts adding jwt authorization

--- app/Routers/user.py
+++ app/Routers/user.py
@@ -31,6 +31,21 @@ async def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
     return db_user
 
+@router.post('/token', response_model=schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = crud.get_user_by_username(db, form_data.username)
+    if not user or not auth.verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = auth.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
 @router.get('/users/', response_model=list[schemas.User])
 async def read_users(skip: int=0, limit: int=0, db: Session = Depends(get_db)):
     users = crud.get_users(db=db, skip=skip, limit=limit)
