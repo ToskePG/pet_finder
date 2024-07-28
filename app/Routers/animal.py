e7e4e4a94953b6e38688071da645ba2b000d7128
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-07-27 22:26:46 -0700

    Fixes some animal endpoints.

--- app/Routers/animal.py
+++ app/Routers/animal.py
@@ -10,7 +10,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 router = APIRouter()
 
-@router.post('/animals', response_model=schemas.Animal)
+@router.post('/create_animal', response_model=schemas.Animal)
 async def create_animal(animal: schemas.AnimalCreate, current_user: schemas.User = Depends(auth.get_current_user), db: Session = Depends(get_db)):
     animal_dict = animal.model_dump()
     db_animal = models.Animal(**animal_dict)
@@ -19,23 +19,21 @@ async def create_animal(animal: schemas.AnimalCreate, current_user: schemas.User
     db.refresh(db_animal)
     return db_animal
 
-@router.get('/animals/id/{animal_id}', response_model = schemas.Animal)
+@router.get('/{animal_id}', response_model = schemas.Animal)
 async def read_animal(animal_id: int, current_user: schemas.User = Depends(auth.get_current_user), db: Session = Depends(get_db)):
     db_animal = crud.get_animal(db=db, animal_id=animal_id)
     if not db_animal:
         raise HTTPException(status_code=404, detail="This animal does not exist")
     return db_animal
 
-@router.get('/animals', response_model= list[schemas.Animal])
+@router.get('/', response_model= list[schemas.Animal])
 async def read_all_animals(animal_type: str = None, animal_breed: str = None, current_user: schemas.User = Depends(auth.get_current_user), db: Session = Depends(get_db)):
     
     db_animals = crud.get_animals(animal_type=animal_type, animal_breed=animal_breed, db=db)
     return db_animals
 
 #Searching animal by species and breed
-@router.get('/animals/type/{animal_type}', response_model=list[schemas.Animal])
+@router.get('/{animal_type}', response_model=list[schemas.Animal])
 async def read_specific_animal_type(animal_type: str, current_user: schemas.User = Depends(auth.get_current_user), db: Session = Depends(get_db)):
     db_animals = crud.get_animals_specific_type(animal_type=animal_type, db=db)
     return db_animals
-
-#@router.delete('/animals/{animal_id}', status_code=)
