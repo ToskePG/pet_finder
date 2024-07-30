e9007ffd2720c6091fbdcbfac4f9b92d91875557
Maksim998 <maksimkontic98@gmail.com>
2024-07-30 19:26:12 +0200

    Animal podels changed to pet models

--- app/database/models.py
+++ app/database/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 import re
 
 
+
 class User(Base):
     __tablename__ = 'users'
 
@@ -12,7 +13,7 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     is_admin = Column(Boolean, default=False)
     password = Column(String)
-    animals = relationship("Animal", cascade = "all, delete-orphan")
+    pets = relationship("Pet", cascade = "all, delete-orphan")
     posts = relationship('Post', cascade = "all, delete-orphan")
 
     @staticmethod
@@ -20,23 +21,23 @@ class User(Base):
         email_regex = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
         return re.match(email_regex, email) is not None
 
-class Animal(Base):
-    __tablename__ = 'animals'
+class Pet(Base):
+    __tablename__ = 'pets'
 
-    animal_id = Column(Integer, primary_key=True)
-    animal_type_id = Column(Integer, ForeignKey("animal_types.animal_type_id"), nullable= False)
-    animal_breed = Column(String, nullable= False)
-    animal_name = Column(String, nullable= False)
-    animal_gender = Column(String, nullable= False)
-    animal_age = Column(Integer, nullable= False)
-    animal_size = Column(String, nullable= False)
-    animal_coatLength = Column(String, nullable= False)
-    animal_color = Column(String, nullable= False)
+    pet_id = Column(Integer, primary_key=True)
+    pet_type_id = Column(Integer, ForeignKey("pet_types.pet_type_id"), nullable= False)
+    pet_breed = Column(String, nullable= False)
+    pet_name = Column(String, nullable= False)
+    pet_gender = Column(String, nullable= False)
+    pet_age = Column(Integer, nullable= False)
+    pet_size = Column(String, nullable= False)
+    pet_coatLength = Column(String, nullable= False)
+    pet_color = Column(String, nullable= False)
     medical_card = Column(String)
     location = Column(Integer, ForeignKey("locations.location_id"), nullable= False)
     user_id = Column(Integer, ForeignKey("users.user_id"), nullable= False)
 
-    animal_type = relationship("AnimalType")
+    pet_type = relationship("PetType")
 
 class Location(Base):
     __tablename__ = "locations"
@@ -44,11 +45,11 @@ class Location(Base):
     location_id = Column(Integer, primary_key=True)
     city_name = Column(String, unique=True)
     
-class AnimalType(Base):
-    __tablename__ = "animal_types"
+class PetType(Base):
+    __tablename__ = "pet_types"
     
-    animal_type_id = Column(Integer, primary_key=True)
-    animal_type = Column(String, unique=True)
+    pet_type_id = Column(Integer, primary_key=True)
+    pet_type = Column(String, unique=True)
 
 class Request(Base):
     __tablename__ = "requests"
@@ -69,7 +70,7 @@ class Post(Base):
 
     post_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.user_id"))
-    animal_id = Column(Integer, ForeignKey("animals.animal_id"))
+    pet_id = Column(Integer, ForeignKey("pets.pet_id"))
     title = Column(String)
     abstract = Column(String)
     content = Column(String)
@@ -79,7 +80,7 @@ class Post(Base):
     post_type_id = Column(Integer, ForeignKey("post_types.post_type_id"), nullable=False)
 
     user = relationship("User", back_populates="posts")
-    animal = relationship("Animal")
+    pet = relationship("Pet")
     post_type = relationship("PostType")
 
 User.posts = relationship("Post", order_by=Post.post_id, back_populates="user")
