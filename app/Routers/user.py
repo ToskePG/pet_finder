9b528c9781dd67a3e3b73262df7757fcbd1725be
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-08-01 01:33:45 -0700

    Adds tags for users.

--- app/Routers/user.py
+++ app/Routers/user.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
-@router.post('/register/', response_model=schemas.User)
+@router.post('/register/', response_model=schemas.User, tags=["User"])
 async def register(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
     logging.info("Starting registration process")
 
@@ -39,7 +39,7 @@ async def register(user: schemas.UserCreate, db: AsyncSession = Depends(get_db))
     logging.info(f"User registered successfully: {db_user.username}")
     return db_user
 
-@router.post('/token', response_model=schemas.Token)
+@router.post('/token', response_model=schemas.Token, tags=["User"])
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
     user = await crud.get_user_by_username_for_token(db, form_data.username)
     if not user or not auth.verify_password(form_data.password, user.password):
@@ -55,12 +55,12 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     return {"access_token": access_token, "token_type": "bearer"}
 
 #Here im trying to verify the currently logged in user using JWT token
-@router.get('/me', response_model=schemas.User)
+@router.get('/me', response_model=schemas.User, tags=["User"])
 async def get_me(current_user: schemas.User = Depends(auth.get_current_user)):
     return current_user
 
 
-@router.get('/{user_id}', response_model=schemas.UserResponse)
+@router.get('/{user_id}', response_model=schemas.UserResponse, tags=["User"])
 async def read_user_by_id(user_id: int, current_user: schemas.User = Depends(auth.get_current_user), db: AsyncSession = Depends(get_db)):
     db_user = await crud.get_user_by_id(user_id=user_id, db=db)
     if not db_user:
@@ -68,7 +68,7 @@ async def read_user_by_id(user_id: int, current_user: schemas.User = Depends(aut
     return db_user
 
 
-@router.get('/{email}', response_model=schemas.UserResponse)
+@router.get('/{email}', response_model=schemas.UserResponse, tags=["User"])
 async def read_user_by_email(user_email: str, current_user: schemas.User = Depends(auth.get_current_user), db: AsyncSession = Depends(get_db)):
     db_user = await crud.get_user_by_email(email=user_email, db=db)
     if not db_user:
@@ -77,7 +77,7 @@ async def read_user_by_email(user_email: str, current_user: schemas.User = Depen
     return db_user
 
 
-@router.get('/{username}', response_model=schemas.UserResponse)
+@router.get('/{username}', response_model=schemas.UserResponse, tags=["User"])
 async def read_user_by_username(username: str, current_user: schemas.User = Depends(auth.get_current_user), db: AsyncSession = Depends(get_db)):
     db_user = await crud.get_user_by_username(username=username, db=db)
     if not db_user:
@@ -86,13 +86,13 @@ async def read_user_by_username(username: str, current_user: schemas.User = Depe
     
 
 #List all users, its more for testing the registration endpoint, but maybe it will still be needed
-@router.get('/', response_model=list[schemas.User])
+@router.get('/', response_model=list[schemas.User], tags=["User"])
 async def read_users(skip: int=0, limit: int=0, db: AsyncSession = Depends(get_db)):
     users = await crud.get_users(db=db, skip=skip, limit=limit)
     return users
 
 
-@router.patch('/{user_id}', response_model = schemas.UserResponse)
+@router.patch('/{user_id}', response_model = schemas.UserResponse, tags=["User"])
 async def update_user(user_id: int, user_update: schemas.UserUpdate, current_user: schemas.User = Depends(auth.get_current_user), db: AsyncSession = Depends(get_db)):
     if user_id != current_user.user_id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to change this user")
@@ -104,7 +104,7 @@ async def update_user(user_id: int, user_update: schemas.UserUpdate, current_use
     return db_user
 
 
-@router.delete('/{user_id}/', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/{user_id}/', status_code=status.HTTP_204_NO_CONTENT, tags=["User"])
 async def delete_user(user_id: int, current_user: schemas.User = Depends(auth.get_current_admin_user), db: AsyncSession = Depends(get_db)):
     
     db_user = await crud.delete_user(user_id=user_id, db=db)
